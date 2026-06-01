f342de3980a089a35f5b51f0d48961c5643e8790
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2026-06-01 21:39:49 +0500

    setup migrations with flyway

---
+++ src/main/java/io/roadmap/filestorage/entity/User.java
@@ -0,0 +1,34 @@
+package io.roadmap.filestorage.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
